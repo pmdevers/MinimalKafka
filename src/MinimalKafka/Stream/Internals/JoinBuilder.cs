a704a193623bc9f0d95d16479acf13ac8606b689
Patrick Evers <pmdevers@gmail.com>
2024-10-15 22:44:28 +0200

    update

--- src/MinimalKafka/Stream/Internals/JoinBuilder.cs
+++ src/MinimalKafka/Stream/Internals/JoinBuilder.cs
@@ -22,44 +22,64 @@ internal class JoinBuilder<K1, V1, K2, V2> : IJoinBuilder<K1, V1, K2, V2>
     public IIntoBuilder<TKey, Tuple<V1?, V2?>> On<TKey>(IStreamStore<TKey, Tuple<V1?, V2?>> store, Func<K1, V1, TKey> leftKey, Func<K2, V2, TKey> rightKey)
     {
         var transformLeftKey = new TransformBlock<Tuple<KafkaContext, K1, V1>,
-            Tuple<KafkaContext, TKey, V1>>((data) =>
-            {
-                var key = leftKey(data.Item2, data.Item3);
-                return Tuple.Create(data.Item1, key, data.Item3);
-            });
+            Tuple<KafkaContext, TKey, V1>>(GetKey(leftKey));
 
         var transformRightKey = new TransformBlock<Tuple<KafkaContext, K2, V2>,
-            Tuple<KafkaContext, TKey, V2>>((data) =>
-            {
-                var key = rightKey(data.Item2, data.Item3);
-                return Tuple.Create(data.Item1, key, data.Item3);
-            });
+            Tuple<KafkaContext, TKey, V2>>(GetKey(rightKey));
+
+       
+        var storeLeft = new TransformBlock<Tuple<KafkaContext, TKey, V1>, Tuple<KafkaContext, TKey, Tuple<V1?, V2?>>>(GetFromStorageV1(store));
+        var storeRight = new TransformBlock<Tuple<KafkaContext, TKey, V2>, Tuple<KafkaContext, TKey, Tuple<V1?, V2?>>>(GetFromStorageV2(store));
 
         _left.LinkTo(transformLeftKey);
         _right.LinkTo(transformRightKey);
-
-        var storeLeft = new TransformBlock<Tuple<KafkaContext, TKey, V1>, Tuple<KafkaContext, TKey, Tuple<V1?, V2?>>>(data =>
-        {
-            var result = store.AddOrUpdate(data.Item2,
-                (k) => Tuple.Create<V1?, V2?>(data.Item3, default),
-                (k, v) => Tuple.Create(data.Item3, v.Item2));
-
-            return Tuple.Create(data.Item1, data.Item2, result);
-        });
-
-        var storeRight = new TransformBlock<Tuple<KafkaContext, TKey, V2>, Tuple<KafkaContext, TKey, Tuple<V1?, V2?>>>(data =>
-        {
-            var result = store.AddOrUpdate(data.Item2,
-                (k) => Tuple.Create<V1?, V2?>(default, data.Item3),
-                (k, v) => Tuple.Create(v.Item1, data.Item3));
-
-            return Tuple.Create(data.Item1, data.Item2, result);
-        });
 
         transformLeftKey.LinkTo(storeLeft);
         transformRightKey.LinkTo(storeRight);
 
         return new IntoBuilder<TKey, Tuple<V1?, V2?>>(storeLeft, storeRight);
+    }
+
+    private static Func<
+        Tuple<KafkaContext, TKey, V2>, 
+        Tuple<KafkaContext, TKey, Tuple<V1?, V2?>>
+        > GetFromStorageV2<TKey>(IStreamStore<TKey, Tuple<V1?, V2?>> store)
+    {
+        return data =>
+        {
+            var result = store.AddOrUpdate(data.Item2,
+                    (k) => Tuple.Create<V1?, V2?>(default, data.Item3),
+                    (k, v) => Tuple.Create<V1?, V2?>(v.Item1, data.Item3));
+            
+            return Tuple.Create(data.Item1, data.Item2, result);
+        };
+    }
+
+    private static Func<
+        Tuple<KafkaContext, TKey, V1>, 
+        Tuple<KafkaContext, TKey, Tuple<V1?, V2?>>
+        > GetFromStorageV1<TKey>(IStreamStore<TKey, Tuple<V1?, V2?>> store)
+    {
+        return data =>
+        {
+            var result = store.AddOrUpdate(data.Item2,
+                (k) => Tuple.Create<V1?, V2?>(data.Item3, default),
+                (k, v) => Tuple.Create<V1?, V2?>(data.Item3, v.Item2));
+
+            return Tuple.Create(data.Item1, data.Item2, result);
+        };
+    }
+
+    private static Func<
+        Tuple<KafkaContext, TKey, TValue>, 
+        Tuple<KafkaContext, TReturn, TValue>
+        > GetKey<TKey, TValue, TReturn>(Func<TKey, TValue, TReturn> keySelector)
+    {
+        return (data) =>
+        {
+            var key = keySelector(data.Item2, data.Item3);
+            return Tuple.Create(data.Item1, key, data.Item3);
+        };
     }
 }
 
