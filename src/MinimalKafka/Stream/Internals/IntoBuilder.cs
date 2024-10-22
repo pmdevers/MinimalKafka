a296eec939a5f9ed284cbf1803f95d76d690e9b1
Patrick Evers <pmdevers@gmail.com>
2024-10-22 20:39:21 +0200

    MetaDataBuilder for streams

--- src/MinimalKafka/Stream/Internals/IntoBuilder.cs
+++ src/MinimalKafka/Stream/Internals/IntoBuilder.cs
@@ -3,18 +3,22 @@
 namespace MinimalKafka.Stream.Internals;
 
 public class IntoBuilder<TKey, TValue>(
+    IWithMetadataBuilder streamBuilder,
     ISourceBlock<Tuple<KafkaContext, TKey, TValue>> left,
     ISourceBlock<Tuple<KafkaContext, TKey, TValue>> right) : IIntoBuilder<TKey, TValue>
 {
+    private readonly IWithMetadataBuilder _streamBuilder = streamBuilder;
     private readonly ISourceBlock<Tuple<KafkaContext, TKey, TValue>> _left = left;
     private readonly ISourceBlock<Tuple<KafkaContext, TKey, TValue>> _right = right;
 
-    public void Into(Func<KafkaContext, TKey, TValue, Task> handler)
+    public IWithMetadataBuilder Into(Func<KafkaContext, TKey, TValue, Task> handler)
     {
         var action = new IntoBlock<TKey, TValue>(handler);
 
         _left.LinkTo(action);
         _right.LinkTo(action);
+
+        return _streamBuilder;
     }
 }
 
