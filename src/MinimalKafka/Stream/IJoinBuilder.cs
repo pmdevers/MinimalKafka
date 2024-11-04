61c990e1c276b0a92bdd844bacd09298ad4ea3e7
Patrick Evers <pmdevers@gmail.com>
2024-11-04 10:19:28 +0100

    On Key Extension Method (#25)
    Co-authored-by: Patrick Evers <patrick.evers@aah.nl>

--- src/MinimalKafka/Stream/IJoinBuilder.cs
+++ src/MinimalKafka/Stream/IJoinBuilder.cs
@@ -7,3 +7,9 @@ public interface IJoinBuilder<K1, V1, K2, V2>
         Func<K2, V2, TKey> rightKey);
 }
 
+public static class JoinBuilderExtensions
+{
+    public static IIntoBuilder<TKey, Tuple<V1?, V2?>> OnKey<TKey, V1, V2>(this IJoinBuilder<TKey, V1, TKey, V2> builder)
+        => builder.On((k1, _) => k1, (k2, _) => k2);
+}
+
