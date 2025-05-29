d53a42add3ec2a69e96f8076725eaee6eccf56cd
Patrick Evers <p.evers@tjip.com>
2025-05-29 14:24:36 +0200

    Rework Completed

--- examples/Examples/Program.cs
+++ examples/Examples/Program.cs
@@ -1,7 +1,6 @@
 using Confluent.Kafka;
 using Examples;
 using MinimalKafka;
-using MinimalKafka.Builders;
 using MinimalKafka.Extension;
 using MinimalKafka.Serializers;
 using MinimalKafka.Stream;
@@ -20,90 +19,32 @@ builder.Services.AddMinimalKafka(config =>
 
 var app = builder.Build();
 
-app.MapStream1<Guid, LeftObject>("left")
+app.MapStream<Guid, LeftObject>("left")
     .Join<int, RightObject>("right").On((l, r) => l.RightObjectId == r.Id)
     .Into((c, v) =>
     {
         var (left, right) = v;
 
         return Task.CompletedTask;
-    });
+    }).WithGroupId("group1");
+
+app.MapStream<Guid, LeftObject>("left")
+    .Into(async (c, k, v) =>
+    {
+        v = v with { RightObjectId = 2 };
+        await c.ProduceAsync("left-update", k, v);
+    }).WithGroupId("group2");
+
+
+app.MapStream<int, RightObject>("right")
+    .Join<Guid, LeftObject>("left").On((k, v) => k, (k, v) => v.RightObjectId)
+    .Into((c, k, v) =>
+    {
+        var (left, right) = v;
+
+        return Task.CompletedTask;
+    })
+    .WithGroupId("group3");
+
 
 await app.RunAsync();
-
-
-
-public class StreamPipelineBuilder<K1, V1, K2, V2>(IKafkaBuilder builder, string left, string right) 
-{
-    private readonly Func<KafkaContext, IStreamStore<K1, V1>> _getLeftStore = (KafkaContext context) 
-        => context.RequestServices.GetRequiredService<IStreamStore<K1, V1>>();
-
-    private readonly Func<KafkaContext, IStreamStore<K2, V2>> _getRightStore = (KafkaContext context) 
-        => context.RequestServices.GetRequiredService<IStreamStore<K2, V2>>();
-
-    private Func<V1, V2, bool> _on = (_, _ ) => true;
-    private Func<KafkaContext, (V1?, V2?), Task> _into = (_, _) => Task.CompletedTask;
-
-    public IKafkaBuilder Builder { get; } = builder;
-    
-    public StreamPipelineBuilder<K1, V1, K2, V2> On(Func<V1, V2, bool> on)
-    {
-        _on = on;
-        return this;
-    }
-
-    public IKafkaConventionBuilder Into(Func<KafkaContext, (V1, V2), Task> into)
-    {
-        _into = into;
-
-        var l = Builder.MapTopic(left, ExecuteLeftAsync);
-        var r = Builder.MapTopic(right, ExecuteRightAsync);
-        return l;
-    }
-
-    public async Task ExecuteLeftAsync(KafkaContext context, K1 key, V1 value)
-    {
-        var leftStore = _getLeftStore(context);
-        var rightStore = _getRightStore(context);
-
-        var left = await leftStore.AddOrUpdate(key, (k) => value, (k, v) => value);
-        var right = await rightStore.FindAsync(x => _on(value, x))
-                              .LastOrDefaultAsync();
-
-        await _into(context, (left, right));
-    }
-
-    public async Task ExecuteRightAsync(KafkaContext context, K2 key, V2 value)
-    {
-        var leftStore = _getLeftStore(context);
-        var rightStore = _getRightStore(context);
-
-        var right = await rightStore.AddOrUpdate(key, (k) => value, (k, v) => value);
-        var left = await leftStore.FindAsync(x => _on(x, value))
-                              .LastOrDefaultAsync();
-
-        await _into(context, (left, right));
-    }
-}
-
-public class StreamPipelineBuilder<K1, V1>(IKafkaBuilder builder, string left)
-{
-    public StreamPipelineBuilder<K1, V1, K2, V2> Join<K2, V2>(string topic)
-    {
-        return new StreamPipelineBuilder<K1, V1, K2, V2>(builder, left, topic);
-    }
-}
-
-public static class Extensions
-{
-    public static StreamPipelineBuilder<K1, V1> MapStream1<K1, V1>(this IApplicationBuilder builder, string topic)
-    {
-        var sb = builder.ApplicationServices.GetRequiredService<IKafkaBuilder>();
-        return sb.MapStream1<K1, V1>(topic);
-    }
-
-    public static StreamPipelineBuilder<K1, V1> MapStream1<K1, V1>(this IKafkaBuilder builder, string topic)
-    {
-        return new StreamPipelineBuilder<K1, V1>(builder, topic);
-    }
-}
--- src/MinimalKafka/Stream/Internals/JoinBuilder.cs
+++ src/MinimalKafka/Stream/Internals/JoinBuilder.cs
@@ -1,43 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using MinimalKafka.Builders;
-using MinimalKafka.Stream.Blocks;
-using System.Threading.Tasks.Dataflow;
+﻿using MinimalKafka.Builders;
 
 namespace MinimalKafka.Stream.Internals;
 
-
-internal class JoinBuilder<K1, V1, K2, V2>(IKafkaBuilder builder,
-    ConsumeBlock<K1, V1> left, string topic) : IJoinBuilder<K1, V1, K2, V2>
+internal class JoinBuilder<K1, V1, K2, V2>(IKafkaBuilder builder, string leftTopic, string rightTopic)
+    : IJoinBuilder<K1, V1, K2, V2>
 {
-    private readonly ConsumeBlock<K1, V1> _left = left;
-    private readonly ConsumeBlock<K2, V2> _right = new(builder, topic);
-
     public IIntoBuilder<(V1, V2)> On(Func<V1, V2, bool> on)
-    {
-        var leftStore = builder.ServiceProvider.GetRequiredService<IStreamStore<K1, V1>>();
-        var rightStore = builder.ServiceProvider.GetRequiredService<IStreamStore<K2, V2>>();
-
-        var join = new JoinBlock<K1, V1, K2, V2>(leftStore, rightStore, on);
-
-        _left.LinkTo(join.Left, new DataflowLinkOptions() {  PropagateCompletion = true });
-        _right.LinkTo(join.Right, new DataflowLinkOptions() { PropagateCompletion = true });
-
-        var joinBuilder = new JoinConventionBuilder(_left.Builder, _right.Builder);
-
-        return new IntoBuilder<(V1, V2)>(joinBuilder, join);
-    }
+        => new JoinIntoBuilder<K1, V1, K2, V2>(builder, leftTopic, rightTopic, on);
 
     public IIntoBuilder<TKey, (V1?, V2?)> On<TKey>(Func<K1, V1, TKey> leftKey, Func<K2, V2, TKey> rightKey)
-    {
-        var store = builder.ServiceProvider.GetRequiredService<IStreamStore<TKey, (V1?, V2?)>>();
-
-        var join = new InnerJoinBlock<TKey, K1, V1, K2, V2>(store, leftKey, rightKey);
-
-        _left.LinkTo(join.Left, new DataflowLinkOptions() { PropagateCompletion = true });
-        _right.LinkTo(join.Right, new DataflowLinkOptions() { PropagateCompletion = true });
-
-        var joinBuilder = new JoinConventionBuilder(_left.Builder, _right.Builder);
-
-        return new IntoBuilder<TKey, (V1?, V2?)>(joinBuilder, join);
-    }
+        => new JoinByKeyIntoBuilder<TKey, K1, V1, K2, V2>(builder, leftTopic, rightTopic, leftKey, rightKey);
 }
