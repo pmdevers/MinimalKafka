f56b33fac352d5d90fd2a0415967911d2d5d163b
Patrick Evers <pmdevers@gmail.com>
2025-07-15 09:17:06 +0200

    Exposes Kafka store extensions
    Makes the Kafka store extension methods public and adds XML documentation.
    
    This allows users to query data from an IKafkaStore using serializers with FindByKey and FindAsync methods.

--- src/MinimalKafka/Stream/KafkaStoreExtensions.cs
+++ src/MinimalKafka/Stream/KafkaStoreExtensions.cs
@@ -2,8 +2,24 @@
 using MinimalKafka.Serializers;
 
 namespace MinimalKafka.Stream;
-internal static class KafkaStoreExtensions
+
+
+/// <summary>
+/// Extension methods for querying data from an <see cref="IKafkaStore"/> using serializers.
+/// </summary>
+public static class KafkaStoreExtensions
 {
+
+    /// <summary>
+    /// Finds and deserializes a value from the store using the provided key.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key to search for.</typeparam>
+    /// <typeparam name="TValue">The type of the value to deserialize.</typeparam>
+    /// <param name="store">The Kafka store instance.</param>
+    /// <param name="key">The key to look up in the store.</param>
+    /// <returns>
+    /// A <typeparamref name="TValue"/> instance if found; otherwise <c>null</c>.
+    /// </returns>
     public async static ValueTask<TValue?> FindByKey<TKey, TValue>(this IKafkaStore store, TKey key)
     {
         var keySerializer = store.ServiceProvider.GetRequiredService<IKafkaSerializer<TKey>>();
@@ -15,7 +31,18 @@ internal static class KafkaStoreExtensions
         return valueSerializer.Deserialize(value);
     }
 
-    public async static IAsyncEnumerable<TValue> FindAsync<TValue>(this IKafkaStore store, Func<TValue, bool> value)
+    /// <summary>
+    /// Asynchronously enumerates all deserialized values from the store that match the given predicate.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the values to retrieve and filter.</typeparam>
+    /// <param name="store">The Kafka store instance.</param>
+    /// <param name="predicate">
+    /// A function to test each value for a condition.
+    /// </param>
+    /// <returns>
+    /// An asynchronous stream of <typeparamref name="TValue"/> instances matching the predicate.
+    /// </returns>
+    public static async IAsyncEnumerable<TValue> FindAsync<TValue>(this IKafkaStore store, Func<TValue, bool> predicate)
     {
         var valueSerializer = store.ServiceProvider.GetRequiredService<IKafkaSerializer<TValue>>();
 
@@ -23,7 +50,7 @@ internal static class KafkaStoreExtensions
         {
             var val = valueSerializer.Deserialize(item);
 
-            if (val is not null && value(val))
+            if (val is not null && predicate(val))
             {
                 yield return val;
             }
