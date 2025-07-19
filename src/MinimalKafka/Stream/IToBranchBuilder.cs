e6f13d7f3c83ad8be019774079184248a607d221
Patrick Evers <pmdevers@gmail.com>
2025-07-19 15:11:50 +0200

    Adds stream branching capabilities (#107)
    Enables branching logic within Kafka streams.
    
    - Introduces extension methods for IBranchBuilder to create branches
    based on selection criteria.
    - Allows configuration of a default branch for processing unmatched
    records, routing them to a specified topic.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    * **New Features**
    * Introduced a fluent branching API for stream processing, allowing
    users to define branches using selector functions.
    * Added the ability to route unmatched records to a specified topic
    asynchronously.
    * Enabled configuration of branch target topics through a builder
    pattern for improved flexibility in stream routing.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ src/MinimalKafka/Stream/IToBranchBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace MinimalKafka.Stream;
+
+/// <summary>
+/// Defines a builder for configuring a branch in a stream processing topology.
+/// </summary>
+/// <remarks>This interface is used to specify the target topic for a branch in the stream processing
+/// topology.</remarks>
+/// <typeparam name="TKey">The type of the key in the stream.</typeparam>
+/// <typeparam name="TValue">The type of the value in the stream.</typeparam>
+public interface IToBranchBuilder<TKey, TValue>
+{
+    /// <summary>
+    /// Specifies the target topic for this branch in the stream processing topology.
+    /// </summary>
+    /// <param name="topicName">The name of the Kafka topic where matching records will be routed.</param>
+    /// <returns>An <see cref="IBranchBuilder{TKey, TValue}"/> to continue building the stream topology.</returns>
+    IBranchBuilder<TKey, TValue> To(string topicName);
+}
